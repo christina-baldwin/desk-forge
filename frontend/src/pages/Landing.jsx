d91d17f56c009c9edfc05c74cfdd35c76a21d329
christina-baldwin <christina.baldwin13@yahoo.com>
2025-06-24 16:48:00 +0200

    adding a logo

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-20 p-50">
-      <h1 className="text-5xl">DeskForge</h1>
+      <img src="/DF-logo.png" alt="Logo" className="w-150" />
       <p className="text-center text-lg/8">
         Welcome to Desk Forge! The app designed for tabletop gamers and
         hobbyists to effortlessly organise their desk spaces. Whether you're
