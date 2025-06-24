bbf7fd4f1a92fb0e65d643d314a8f303301de787
christina-baldwin <christina.baldwin13@yahoo.com>
2025-06-24 16:34:21 +0200

    basic styling

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <>
-      <h1>DeskForge</h1>
-      <p>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-20 p-50">
+      <h1 className="text-5xl">DeskForge</h1>
+      <p className="text-center text-lg">
         Welcome to Desk Forge! The app designed for tabletop gamers and
         hobbyists to effortlessly organise their desk spaces. Whether you're
         managing your Warhammer armies or juggling multiple creative projects,
@@ -12,15 +12,19 @@ const Landing = () => {
         notes, get AI-powered suggestions, and spend more time doing what you
         love instead of stressing about organisation!
       </p>
-      <div>
+      <div className="flex gap-8">
         <Link to="/login">
-          <button>Login</button>
+          <button className="px-3 py-2 border-2 rounded-[5px] cursor-pointer">
+            Login
+          </button>
         </Link>
         <Link to="/register">
-          <button>Register</button>
+          <button className="px-3 py-2 border-2 rounded-[5px] cursor-pointer">
+            Register
+          </button>
         </Link>
       </div>
-    </>
+    </div>
   );
 };
 
