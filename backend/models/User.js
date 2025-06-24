e813557a74c8a1d2e493ae18a36fa0aa06663e7a
christina-baldwin <christina.baldwin13@yahoo.com>
2025-06-24 14:31:33 +0200

    setting up folders for authorisation and setting up the user model

---
+++ backend/models/User.js
@@ -0,0 +1,27 @@
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true, minlength: 3 },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true, minlength: 6 },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
