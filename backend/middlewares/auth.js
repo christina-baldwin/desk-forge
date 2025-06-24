02224f9572990f2dfcc86cdc6df98cacd27ee0ed
christina-baldwin <christina.baldwin13@yahoo.com>
2025-06-24 14:35:53 +0200

    setting up the auth.js middleware

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const authenticate = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res.status(401).json({ message: "Authorisation header missing" });
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Token missing" });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Invalid or expired token" });
+  }
+};
