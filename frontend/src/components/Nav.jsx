4847f26c9ea465ab8653c0df0a1f769860315686
christina-baldwin <christina.baldwin13@yahoo.com>
2025-06-30 11:19:39 +0200

    getting the logout to work

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,18 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const apiUrl = "http://localhost:8080";
 
 const Nav = () => {
   const [userName, setUserName] = useState("");
   const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -39,7 +47,10 @@ const Nav = () => {
         <img src="/DF-logo-simple.png" alt="Logo" className="w-8" />
         <p>Welcome {userName || "..."}!</p>
       </div>
-      <button className="px-3 py-2 border-2 rounded-[5px] cursor-pointer">
+      <button
+        onClick={handleLogout}
+        className="px-3 py-2 border-2 rounded-[5px] cursor-pointer"
+      >
         Logout
       </button>
     </div>
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const Dashboard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      navigate("/login");
+      return;
+    }
+  }, [navigate]);
+
   return (
     <div>
       <h1>My dashboard</h1>
