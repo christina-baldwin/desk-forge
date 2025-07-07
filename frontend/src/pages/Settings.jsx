13b3ebb6beb7c89bb08d395444f441e342db7ac3
christina-baldwin <christina.baldwin13@yahoo.com>
2025-07-07 16:03:37 +0200

    starting the editing functionality. TODO patch on the backend

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -5,7 +5,17 @@ const apiUrl = "http://localhost:8080";
 
 const Settings = () => {
   const [userName, setUserName] = useState("");
+  const [isEditingName, setIsEditingName] = useState(false);
+  const [newName, setNewName] = useState(userName);
+
   const [userEmail, setUserEmail] = useState("");
+  const [isEditingEmail, setIsEditingEmail] = useState(false);
+  const [newEmail, setNewEmail] = useState("");
+
+  const [isEditingPassword, setIsEditingPassword] = useState(false);
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -36,13 +46,56 @@ const Settings = () => {
     fetchUser();
   }, []);
 
+  const handleNameSave = async () => {
+    const token = localStorage.getItem("token");
+
+    //TODO: need a patch route on the backend
+    try {
+      const response = await fetch(`${apiUrl}/auth/user`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ name: newName }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message || "Failed to update user name");
+      }
+
+      setUserName(newName);
+      setIsEditingName(false);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="flex gap-4">
       <SideBar />
       <div className="flex-1 p-4">
         <h1 className="text-2xl font-bold">Settings</h1>
         <h3>Name</h3>
-        <p>{userName}</p>
+        {isEditingName ? (
+          <>
+            <input
+              type="text"
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+            />
+            <button onClick={handleNameSave}>Save</button>
+            <button onClick={() => setIsEditingName(false)}>Cancel</button>
+          </>
+        ) : (
+          <>
+            <p>{userName}</p>
+            <button onClick={() => setIsEditingName(true)}>Change Name</button>
+          </>
+        )}
+
         <h3>Email</h3>
         <p>{userEmail}</p>
         <button className="px-3 py-2 border-2 rounded-[5px] cursor-pointer">
