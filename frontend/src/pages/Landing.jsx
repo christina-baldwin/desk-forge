33c7129d0f6e77ee92967edfe81214de30624ba7
christina-baldwin <christina.baldwin13@yahoo.com>
2025-07-29 14:49:27 +0200

    finialising design for landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen gap-10 p-30 bg-teal-900">
+    <div className="flex flex-col items-center min-h-screen gap-10 p-30 bg-[#004d40]">
       <div className="flex gap-30 mb-10">
         <Link to="/login">
-          <button className="px-4 py-2 bg-[#4fb2ad] text-black text-lg border-black border-3 rounded cursor-pointer font-mono drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
-            Login
+          <button className="px-4 py-2 bg-slate-100 text-black text-lg rounded cursor-pointer font-mono border-[#e76f51] border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
+            LOGIN
           </button>
         </Link>
         <div className="flex items-center justify-center">
@@ -18,19 +18,26 @@ const Landing = () => {
           </h1>
         </div>
         <Link to="/register">
-          <button className="px-4 py-2 bg-slate-100 text-black text-lg border-black border-3 rounded font-sans cursor-pointer drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
-            Register
+          <button className="px-4 py-2 bg-[#e76f51] text-black text-lg rounded font-mono cursor-pointer border-slate-100 border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
+            REGISTER
           </button>
         </Link>
       </div>
-      <div className="flex flex-col items-center bg-slate-100 p-60 border-10 rounded-lg shadow-lg">
+      <div className="flex flex-col items-center bg-slate-100 pt-30 pl-50 pr-50 border-10 rounded-lg shadow-lg">
         <h2 className="font-mono text-6xl mb-15 text-textDark text-center">
-          Smarter Desks. <br /> Smarter Hobbies.
+          Smarter Desks. Smarter Hobbies.
         </h2>
         <p className="font-sans text-center text-xl/10 p-8 mb-12">
-          AI-powered feedback to improve your hobby desk setup. Tailored for
-          miniature painting, gaming, and more.
+          AI-powered feedback to optimise your hobby desk setup. Engineered for
+          building and painting. Elevate your crafting station with tailored
+          insights designed to enhance precision, comfort, and creativity for
+          every step of your creative journey.
         </p>
+        <img
+          src="/hero-img-crop.png"
+          alt="Screenshot of app dashboard"
+          className="w-70"
+        />
       </div>
     </div>
   );
