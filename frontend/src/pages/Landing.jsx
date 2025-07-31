b2f7a29f7db5ca85db40d3de01739f5805a787f1
christina-baldwin <christina.baldwin13@yahoo.com>
2025-07-31 15:21:50 +0200

    setting up colour classes in tailwind for base theme colours

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,29 +2,29 @@ import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
-    <div className="flex flex-col items-center min-h-screen gap-10 p-30 bg-[#004d40]">
+    <div className="flex flex-col items-center min-h-screen gap-10 p-30 bg-primary text-dark">
       <div className="flex gap-30 mb-10">
         <Link to="/login">
-          <button className="px-4 py-2 bg-slate-100 text-black text-lg rounded cursor-pointer font-mono border-[#e76f51] border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
+          <button className="px-4 py-2 bg-light text-dark text-lg rounded cursor-pointer font-mono border-accent border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
             LOGIN
           </button>
         </Link>
         <div className="flex items-center justify-center">
           <h1
-            className="text-slate-100 text-4xl drop-shadow-[3px_3px_0_#000000]"
+            className="text-light text-4xl drop-shadow-[3px_3px_0_#000000]"
             style={{ fontFamily: "'Bungee', cursive" }}
           >
             DeskForge
           </h1>
         </div>
         <Link to="/register">
-          <button className="px-4 py-2 bg-[#e76f51] text-black text-lg rounded font-mono cursor-pointer border-slate-100 border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
+          <button className="px-4 py-2 bg-accent text-dark text-lg rounded font-mono cursor-pointer border-light border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200">
             REGISTER
           </button>
         </Link>
       </div>
-      <div className="flex flex-col items-center bg-slate-100 pt-30 pl-50 pr-50 border-10 rounded-lg shadow-lg">
-        <h2 className="font-mono text-6xl mb-15 text-textDark text-center">
+      <div className="flex flex-col items-center bg-light pt-30 pl-50 pr-50 border-10 rounded-lg shadow-lg">
+        <h2 className="font-mono text-6xl mb-15 text-dark text-center">
           Smarter Desks. Smarter Hobbies.
         </h2>
         <p className="font-sans text-center text-xl/10 p-8 mb-12">
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,12 +3,6 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html", "./**/*.html"],
   theme: {
     extend: {
-      colors: {
-        primary: "#0e5d5a",
-        accent: "#3ed2b3",
-        background: "#e8f0f2",
-        textDark: "#1b2a2f",
-      },
       fontFamily: {
         sans: ["Inter", "sans-serif"],
         mono: ["Share Tech Mono", "monospace"],
