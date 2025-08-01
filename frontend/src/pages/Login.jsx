32701e5fce7b2b3305e63bc2da40c453c7fc051f
christina-baldwin <christina.baldwin13@yahoo.com>
2025-08-01 13:00:36 +0200

    styling the login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -51,74 +51,76 @@ const Login = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-15">
-      <h2 className="font-mono text-5xl">Log in</h2>
-      <form
-        className="border-4 border-cyan-600 rounded px-20 pb-12 pt-4"
-        onSubmit={handleLogin}
-      >
-        <div className="h-6 mb-2 w-full max-w-xs overflow-hidden">
-          <p
-            className={`text-sm text-red-600 transition-opacity duration-300 ${
-              error ? "opacity-100" : "opacity-0"
-            }`}
+      <h1 className="font-logo text-light text-4xl drop-shadow-[3px_3px_0_#000000]">
+        DeskForge
+      </h1>
+      <div className="flex flex-col items-center gap-8 bg-light pt-30 pb-30 pl-60 pr-60 border-10 rounded-lg shadow-lg">
+        <h2 className="font-heading text-5xl">Log in</h2>
+        <form onSubmit={handleLogin}>
+          <div className="h-6 mb-2 w-full max-w-xs overflow-hidden">
+            <p
+              className={`text-sm text-red-600 transition-opacity duration-300 ${
+                error ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              {error || "placeholder"}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2 mb-4">
+            <label className="font-heading" htmlFor="email">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="you@example.com"
+              onChange={(e) => setEmail(e.target.value)}
+              className="font-body px-2 py-1 border-1 rounded-[5px]"
+            />
+          </div>
+          <div className="flex flex-col gap-2 mb-4">
+            <label className="font-heading" htmlFor="password">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="••••••••"
+              onChange={(e) => setPassword(e.target.value)}
+              className="font-body px-2 py-1 border-1 rounded-[5px]"
+            />
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-accent text-dark text-lg rounded font-heading cursor-pointer border-light border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200"
           >
-            {error || "placeholder"}
-          </p>
-        </div>
-        <div className="flex flex-col gap-2 mb-4">
-          <label className="font-sans" htmlFor="email">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="you@example.com"
-            onChange={(e) => setEmail(e.target.value)}
-            className="px-2 py-1 border-1 rounded-[5px]"
-          />
-        </div>
-        <div className="flex flex-col gap-2 mb-4">
-          <label className="font-sans" htmlFor="password">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="••••••••"
-            onChange={(e) => setPassword(e.target.value)}
-            className="px-2 py-1 border-1 rounded-[5px]"
-          />
-        </div>
-        <button
-          type="submit"
-          className="px-4 py-2 bg-cyan-600 text-white text-lg border-black border-2 rounded cursor-pointer hover:bg-cyan-700 font-mono transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200"
-        >
-          Log In
-        </button>
-      </form>
+            Log In
+          </button>
+        </form>
 
-      <p className="mt-4 text-lg font-sans">
-        Don’t have an account?{" "}
-        <Link to="/register" className="underline">
-          Register here
+        <p className="mt-4 text-lg italic font-body">
+          Don’t have an account?{" "}
+          <Link to="/register" className="underline">
+            Register here
+          </Link>
+        </p>
+        <Link
+          to="/"
+          className="mt-4 text-lg font-heading text-dark font-bold hover:underline duration:200 transition-all"
+        >
+          ← Back to Home
         </Link>
-      </p>
-      <Link
-        to="/"
-        className="mt-4 text-lg font-sans text-cyan-600 font-bold hover:underline duration:200 transition-all"
-      >
-        ← Back to Home
-      </Link>
-      <div className="h-6 mb-2 w-full max-w-xs overflow-hidden">
-        {isLoading && (
-          <p
-            className={`text-sm text-center italic transition-opacity duration-300 ${
-              isLoading ? "opacity-100" : "opacity-0"
-            }`}
-          >
-            Logging in...
-          </p>
-        )}
+        <div className="h-6 mb-2 w-full max-w-xs overflow-hidden">
+          {isLoading && (
+            <p
+              className={`font-heading text-dark text-sm text-center italic transition-opacity duration-300 ${
+                isLoading ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              Logging in...
+            </p>
+          )}
+        </div>
       </div>
     </div>
   );
