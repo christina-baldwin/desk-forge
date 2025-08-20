55452a7b8bd56cdee1f09bbdd0f5cb33f5732b26
christina-baldwin <christina.baldwin13@yahoo.com>
2025-08-20 16:06:08 +0200

    fixing some styling

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -95,11 +95,14 @@ const Dashboard = () => {
     <div className="flex flex-col md:flex-row gap-4 p-4 md:p-8">
       <SideBar />
 
-      <div className="bg-light border-6 rounded-lg shadow-lg flex flex-col gap-8 p-6 md:p-10 md:w-[720px] lg:w-[900px] mx-auto md:mx-0 overflow-auto">
+      <div
+        className="bg-light border-6 rounded-lg shadow-lg flex flex-col gap-8 
+                  p-4 sm:p-6 md:p-10 w-full md:w-[720px] lg:w-[900px] 
+                  mx-auto md:mx-0 overflow-auto"
+      >
         <h1 className="font-heading text-dark text-3xl sm:text-4xl md:text-4xl font-bold text-center md:text-left">
           Welcome Back!
         </h1>
-
         <div className="text-center md:text-left">
           <p className="font-body text-dark">
             Last logged in: {formatDate(previousLogin) || formatDate(lastLogin)}
@@ -108,24 +111,22 @@ const Dashboard = () => {
             You last uploaded a desk photo {formatDate(desk?.createdAt)}!
           </p>
         </div>
-
         <div className="mb-4 px-5 py-6 border-2 text-dark font-body rounded-[5px]">
           <h2 className="text-lg font-heading text-dark mb-2">
             Summary of Your Latest Desk Setup
           </h2>
           <p className="font-body text-dark">{desk?.summary}</p>
         </div>
-
         <div>
           <h2 className="font-heading text-center md:text-left text-dark text-xl font-bold mb-2">
             Latest Desk Setup
           </h2>
           {uploadedUrl ? (
-            <div className="mt-4 flex justify-center md:justify-start">
+            <div className="mt-4 p-10 flex justify-center md:justify-start">
               <img
                 src={uploadedUrl}
                 alt="Uploaded preview"
-                className="max-w-xs max-h-64 border-accent border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] rounded-lg"
+                className="max-w-2xs sm:max-w-xs max-h-64 border-accent border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] rounded-lg"
               />
             </div>
           ) : (
@@ -134,17 +135,16 @@ const Dashboard = () => {
             </p>
           )}
         </div>
-
         <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start mt-4">
           <Link
             to="/upload"
-            className="px-4 py-2 bg-accent text-dark text-md rounded font-heading cursor-pointer border-light border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200 focus:outline-none focus:ring-2 focus:ring-accent"
+            className="px-4 py-2 text-center bg-accent text-dark text-md rounded font-heading cursor-pointer border-light border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200 focus:outline-none focus:ring-2 focus:ring-accent"
           >
             Analyse Desk
           </Link>
           <Link
             to="/suggestions"
-            className="px-4 py-2 bg-light text-dark text-md rounded cursor-pointer font-heading border-accent border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200 focus:outline-none focus:ring-2 focus:ring-accent"
+            className="px-4 py-2 text-center bg-light text-dark text-md rounded cursor-pointer font-heading border-accent border-4 shadow-[0_0_0_4px_black] drop-shadow-[3px_3px_0_#1b2a2f] transform hover:translate-x-1 hover:translate-y-1 transition-transform duration-200 focus:outline-none focus:ring-2 focus:ring-accent"
           >
             View Suggestions
           </Link>
